f4fdc83f8eaf4e8326d814806a22f63102c961aa
RRRhapkala <grixagrodnoblr@gmail.com>
2026-03-06 12:47:48 +0100

    added middleware for admin

--- handlers.go
+++ handlers.go
@@ -15,6 +15,7 @@ func GetAllVehiclesHandler(c *gin.Context) {
 	retVal, err := GetAllVehicles()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
 	c.JSON(http.StatusOK, retVal)
 }
